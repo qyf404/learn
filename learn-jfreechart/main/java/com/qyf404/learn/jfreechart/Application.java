a6e3f4bb1f8164e6be965e6cda11eaf2aa11cda9
QiYifei <yfqi@thoughtworks.com>
2017-09-03 09:29:33 +0800

    add first chart

---
+++ learn-jfreechart/main/java/com/qyf404/learn/jfreechart/Application.java
@@ -0,0 +1,56 @@
+package com.qyf404.learn.jfreechart;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        // create a dataset...
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("Category 1", 43.2);
+        dataset.setValue("Category 2", 27.9);
+        dataset.setValue("Category 3", 79.5);
+
+        // create a chart...
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Sample Pie Chart",
+                dataset,
+                true, // legend?
+                true, // tooltips?
+                false // URLs?
+        );
+
+        // create and display a frame...
+        ChartFrame frame = new ChartFrame("First", chart);
+        frame.pack();
+        frame.setVisible(true);
+
+        exportImage(frame.getChartPanel(), "/tmp/abc.png");
+    }
+
+    private static void exportImage(JPanel panel, String pathname) throws IOException {
+        //create a new image
+        BufferedImage image = new BufferedImage(panel.getWidth(), panel.getHeight(),
+                BufferedImage.TYPE_INT_ARGB);
+
+        //paint the editor onto the image
+        SwingUtilities.paintComponent(image.createGraphics(),
+                panel,
+                new JPanel(),
+                0, 0, image.getWidth(), image.getHeight());
+
+        //save the image to file
+        ImageIO.write((RenderedImage)image, "png", new File(pathname));
+    }
+
+
+}
