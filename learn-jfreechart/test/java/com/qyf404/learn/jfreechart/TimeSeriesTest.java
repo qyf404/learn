d2dd27ef43b1d26f4e94c42775b418b9feaeeb30
QiYifei <yfqi@thoughtworks.com>
2017-09-03 15:59:37 +0800

    add customer time series chart

--- learn-jfreechart/test/java/com/qyf404/learn/jfreechart/TimeSeriesTest.java
+++ learn-jfreechart/test/java/com/qyf404/learn/jfreechart/TimeSeriesTest.java
@@ -2,13 +2,23 @@ package com.qyf404.learn.jfreechart;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.DateAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.time.Month;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RectangleInsets;
+import org.jfree.ui.RefineryUtilities;
 import org.junit.Test;
 
+import java.awt.*;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 
 public class TimeSeriesTest {
     @Test
@@ -72,5 +82,92 @@ public class TimeSeriesTest {
 //        frame.setVisible(true);
 
         ImageUtil.exportImage(frame.getChartPanel(), "/tmp/jfreechart_hello_time_series.png");
+    }
+
+    @Test
+    public void customer_time_series() throws IOException {
+        // ready for data
+        TimeSeries s1 = new TimeSeries("L&G European Index Trust", Month.class);
+        s1.add(new Month(2, 2001), 181.8);
+        s1.add(new Month(3, 2001), 167.3);
+        s1.add(new Month(4, 2001), 153.8);
+        s1.add(new Month(5, 2001), 167.6);
+        s1.add(new Month(6, 2001), 158.8);
+        s1.add(new Month(7, 2001), 148.3);
+        s1.add(new Month(8, 2001), 153.9);
+        s1.add(new Month(9, 2001), 142.7);
+        s1.add(new Month(10, 2001), 123.2);
+        s1.add(new Month(11, 2001), 131.8);
+        s1.add(new Month(12, 2001), 139.6);
+        s1.add(new Month(1, 2002), 142.9);
+        s1.add(new Month(2, 2002), 138.7);
+        s1.add(new Month(3, 2002), 137.3);
+        s1.add(new Month(4, 2002), 143.9);
+        s1.add(new Month(5, 2002), 139.8);
+        s1.add(new Month(6, 2002), 137.0);
+        s1.add(new Month(7, 2002), 132.8);
+        TimeSeries s2 = new TimeSeries("L&G UK Index Trust", Month.class);
+        s2.add(new Month(2, 2001), 129.6);
+        s2.add(new Month(3, 2001), 123.2);
+        s2.add(new Month(4, 2001), 117.2);
+        s2.add(new Month(5, 2001), 124.1);
+        s2.add(new Month(6, 2001), 122.6);
+        s2.add(new Month(7, 2001), 119.2);
+        s2.add(new Month(8, 2001), 116.5);
+        s2.add(new Month(9, 2001), 112.7);
+        s2.add(new Month(10, 2001), 101.5);
+        s2.add(new Month(11, 2001), 106.1);
+        s2.add(new Month(12, 2001), 110.3);
+        s2.add(new Month(1, 2002), 111.7);
+        s2.add(new Month(2, 2002), 111.0);
+        s2.add(new Month(3, 2002), 109.6);
+        s2.add(new Month(4, 2002), 113.2);
+        s2.add(new Month(5, 2002), 111.6);
+        s2.add(new Month(6, 2002), 108.8);
+        s2.add(new Month(7, 2002), 101.6);
+        TimeSeriesCollection dataset = new TimeSeriesCollection();
+        dataset.addSeries(s1);
+        dataset.addSeries(s2);
+        dataset.setDomainIsPointsInTime(true);
+
+        // create chart
+        JFreeChart chart = ChartFactory.createTimeSeriesChart(
+                "Legal & General Unit Trust Prices",  // title
+                "Date",// x-axis label
+                "Price Per Unit",// y-axis label
+                dataset,// data
+                true,// create legend?
+                true,// generate tooltips?
+                false// generate URLs?
+        );
+        chart.setBackgroundPaint(Color.white);
+        XYPlot plot = (XYPlot) chart.getPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        plot.setAxisOffset(new RectangleInsets(5.0, 5.0, 5.0, 5.0));
+        plot.setDomainCrosshairVisible(true);
+        plot.setRangeCrosshairVisible(true);
+        XYItemRenderer r = plot.getRenderer();
+        if (r instanceof XYLineAndShapeRenderer) {
+            XYLineAndShapeRenderer renderer = (XYLineAndShapeRenderer) r;
+            renderer.setBaseShapesVisible(true);
+            renderer.setBaseShapesFilled(true);
+        }
+        DateAxis axis = (DateAxis) plot.getDomainAxis();
+        axis.setDateFormatOverride(new SimpleDateFormat("MMM-yyyy"));
+
+        // create chartPanel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(500, 270));
+        chartPanel.setMouseZoomable(true, false);
+
+        ApplicationFrame frame = new ApplicationFrame("Time Series Demo 1");
+        frame.setContentPane(chartPanel);
+        frame.pack();
+        RefineryUtilities.centerFrameOnScreen(frame);
+        frame.setVisible(true);
+
+        ImageUtil.exportImage(chartPanel, "/tmp/jfreechart_customer_time_series.png");
     }
 }
